f7c41a69883b6604e063b2e9155ab05c01e42107
Christian <christian.eckard@googlemail.com>
2022-09-17 12:07:09 +0200

    Add click and resize

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Based on
 https://scikit-image.org/docs/stable/auto_examples/segmentation/plot_boundary_merge.html#sphx-glr-auto-examples-segmentation-plot-boundary-merge-py.
+
+## Usage
+Example:
+```
+python convert.py --path=<image.jpg> -g0 -c30 -s50 -t0.01
+```
--- convert.py
+++ convert.py
@@ -1,8 +1,13 @@
-from skimage.future import graph
-from skimage import data, segmentation, color, filters, io
-from matplotlib import pyplot as plt
 import sys
+import numpy as np
+import click
+from skimage import data, segmentation, color, filters, io
+from skimage.transform import resize
+from skimage.future import graph
+from matplotlib import pyplot as plt
 from itertools import product
+from PIL import Image
+
 
 def weight_boundary(graph, src, dst, n):
     """
@@ -29,18 +34,18 @@ def weight_boundary(graph, src, dst, n):
         assigned for the merged node.
 
     """
-    default = {'weight': 0.0, 'count': 0}
+    default = {"weight": 0.0, "count": 0}
 
-    count_src = graph[src].get(n, default)['count']
-    count_dst = graph[dst].get(n, default)['count']
+    count_src = graph[src].get(n, default)["count"]
+    count_dst = graph[dst].get(n, default)["count"]
 
-    weight_src = graph[src].get(n, default)['weight']
-    weight_dst = graph[dst].get(n, default)['weight']
+    weight_src = graph[src].get(n, default)["weight"]
+    weight_dst = graph[dst].get(n, default)["weight"]
 
     count = count_src + count_dst
     return {
-        'count': count,
-        'weight': (count_src * weight_src + count_dst * weight_dst)/count
+        "count": count,
+        "weight": (count_src * weight_src + count_dst * weight_dst) / count,
     }
 
 
@@ -52,56 +57,83 @@ def merge_boundary(graph, src, dst):
     pass
 
 
-def convert(path, compactness=30, segments=400, thresh=0.08):
+@click.command()
+@click.option("-p", "--path", help="Path to image file")
+@click.option("-g", "--grid", default=True, type=bool, help="Generate a parameter grid")
+@click.option("-c", "--compactness", default=30, type=int)
+@click.option("-s", "--segments", default=400, type=int)
+@click.option("-t", "--thresh", default=0.08, type=float)
+def convert(path, grid, compactness, segments, thresh):
+    if grid:
+        convert_grid(path)
+    else:
+        img = _convert(path, compactness, segments, thresh)
+        show(img)
+        save(img, "out.png")
+
+
+def _convert(path, compactness, segments, thresh):
     img = io.imread(path)
+    img = resize(img, (400, 400))
     edges = filters.sobel(color.rgb2gray(img))
-    labels = segmentation.slic(img, compactness=compactness, n_segments=segments, start_label=1)
+    labels = segmentation.slic(
+        img, compactness=compactness, n_segments=segments, start_label=1
+    )
     g = graph.rag_boundary(labels, edges)
 
     # graph.show_rag(labels, g, img)
     # plt.title('Initial RAG')
 
-    labels2 = graph.merge_hierarchical(labels, g, thresh=THRESH, rag_copy=False,
-                                       in_place_merge=True,
-                                       merge_func=merge_boundary,
-                                       weight_func=weight_boundary)
+    labels2 = graph.merge_hierarchical(
+        labels,
+        g,
+        thresh=thresh,
+        rag_copy=False,
+        in_place_merge=True,
+        merge_func=merge_boundary,
+        weight_func=weight_boundary,
+    )
 
     # graph.show_rag(labels, g, img)
     # plt.title('RAG after hierarchical merging')
 
-    out = color.label2rgb(labels2, img, kind='avg', bg_label=0)
+    out = color.label2rgb(labels2, img, kind="avg", bg_label=0)
 
     return out
+
 
 def show(img):
     plt.figure()
     plt.imshow(img)
-    plt.title('Final segmentation')
     plt.show()
 
+
+def save(img, name):
+    img = (img * 255).astype(np.uint8)
+    im = Image.fromarray(img).convert("RGB")
+    im.save(name)
+
+
 def convert_grid(path):
-    comp = (10, 20, 30, 40)
-    seg = (400, 500, 600, 700, 800)
-    thr = (0.08, 0.04, 0.01)
+    comp = (10, 30, 60)
+    seg = (50, 100, 300)
+    thr = (0.001, 0.005, 0.01)
 
-    fig, axs = plt.subplots(nrows=len(seg), ncols=len(comp)*len(thr), figsize=(12, 8),
-                            subplot_kw={'xticks': [], 'yticks': []})
+    fig, axs = plt.subplots(
+        nrows=len(comp),
+        ncols=len(seg) * len(thr),
+        figsize=(12, 8),
+        subplot_kw={"xticks": [], "yticks": []},
+    )
 
-    for (compactness, segments, thresh), ax in zip(product(comp, seg, thr), axs.flat):
-        img = convert(path, compactness, segments, thresh)
+    for (thresh, segments, compactness), ax in zip(product(thr, seg, comp), axs.flat):
+        img = _convert(path, compactness, segments, thresh)
         ax.imshow(img)
-        ax.set_title(f"C,S,T=({compactness}, {segments}, {thresh})")
+        ax.set_title(f"C{compactness}S{segments}T{thresh}")
 
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
-    PATH = sys.argv[1]
-    COMPACTNESS = int(sys.argv[2]) # 30
-    SEGMENTS = int(sys.argv[3]) # 400
-    THRESH = float(sys.argv[4]) # 0.08
-
-    #show(convert(PATH, COMPACTNESS, SEGMENTS, THRESH))
-
-    convert_grid(PATH)
+    convert()
